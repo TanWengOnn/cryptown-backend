4312c4bee11a4f388b02e623fe94074d68530869
Tan Weng Onn <weng.onn@besquare.com.my>
2022-11-04 08:42:45 +0000

    updated crypto detail chart response format and change chart for myr to usd, brute force protection not complete

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -5,7 +5,7 @@ const getNewsList = async (req, res) => {
     try {
         let news = await getBingNews()
         // send a json response
-        res.status(400).json({mssg: "GET News Lists successful", news})
+        res.status(200).json({mssg: "GET News Lists successful", news})
     } catch (error) {
         res.status(400).json({
             mssg: "Failed to get news list", 

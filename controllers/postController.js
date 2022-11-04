f498e16954946aa0561bc45f4a23fd3dda8cfdb2
Tan Weng Onn <weng.onn@besquare.com.my>
2022-11-04 07:13:32 +0000

    forum API complete

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,25 +1,35 @@
-const { queryDb }= require("../db_config/db")
-const validator = require("validator")
-const { v4: uuidv4 } = require('uuid');
+const {
+    getPosts,
+    addPost,
+    addSubPost
+} = require("./functions/postFunctions")
 
 const getPost = async (req, res) => {
     const userId = req.userId
 
-    let result = await getPosts_2(userId)
-    let posts = result["getPost"]
-    let subPosts = result["getSubPost"]
-    let postsObj={};
-    for(const post of posts){
-        postsObj[post.postid]=post;
-        post["replies"]=[]
-    }
+    try {
+        let result = await getPosts(userId)
+        let posts = result["getPost"]
+        let subPosts = result["getSubPost"]
+        let postsObj={};
+        for(const post of posts){
+            postsObj[post.postid]=post;
+            post["replies"]=[]
+        }
+        
+        for(const subpost of subPosts){
+            postsObj[subpost["postid"]]["replies"].push(subpost);
+        }
     
-    for(const subpost of subPosts){
-        postsObj[subpost["postid"]]["replies"].push(subpost);
+        // send a json response
+        res.status(200).json({mssg: "GET posts from docker", postsObj})
+    } catch (error) {
+        res.status(400).json({
+            mssg: "Failed to get Post",
+            error: error.message
+        })
     }
-
-    // send a json response
-    res.json({mssg: "GET posts from docker", postsObj})
+   
 }
 
 const addPosts = async (req, res) => {
@@ -52,193 +62,6 @@ const addSubPosts = async (req, res) => {
     }    
 }
 
-// const getPosts = async function(userId) {
-//     let escaped_userId = validator.escape(userId)
-
-//     let checkUser = {
-//         text: "select * from cryptown.users where userid=$1",
-//         values: [escaped_userId]
-//     }
-
-//     let user = await queryDb(checkUser)
-
-//     if (user["result"].length === 0) {
-//         throw Error('User does not exist')
-//     }
-
-//     let getPost = {
-//         text: 
-//         `with subposts as 
-//         (select * from cryptown.subposts)
-//         , posts as 
-//             (select 
-//                 p.userid as userid
-//                 , sp.userid as subpostuserid
-//                 , sp.postid as postid
-//                 , p.postid as postid_1
-//                 , p.post as post
-//                 , sp.subpost as subpost
-//             from subposts as sp 
-//                 left join 
-//             cryptown.posts as p 
-//                 on sp.postid=p.postid)
-//         select 
-//             p.userid
-//             , users.email
-//             , p.subpostuserid
-//             , p.postid
-//             , p.postid_1
-//             , p.post
-//             , p.subpost 
-//         from posts as p 
-//             left join 
-//         cryptown.users as users
-//             on p.userid=users.userid;`,
-//         // values: [postId, escaped_userId, escaped_post, escaped_dateTime,server_datetime]
-//     }
-
-//     let getQuery = await queryDb(getPost)
-
-//     if (getQuery["error"] !== undefined) {
-//         throw Error("Failed to get post")
-//     }
-    
-//     return getQuery["result"]
-
-// }
-
-const getPosts_2 = async function(userId) {
-    let escaped_userId = validator.escape(userId)
-
-    let checkUser = {
-        text: "select * from cryptown.users where userid=$1",
-        values: [escaped_userId]
-    }
-
-    let user = await queryDb(checkUser)
-
-    if (user["result"].length === 0) {
-        throw Error('User does not exist')
-    }
-
-    let getPostQuery = {
-        text: 
-        `select posts.postid, posts.post, posts.postdatetime, users.email from cryptown.posts as posts left join cryptown.users as users on posts.userid=users.userid;`,
-        // values: [postId, escaped_userId, escaped_post, escaped_dateTime,server_datetime]
-    }
-
-    let getPost = await queryDb(getPostQuery)
-
-    if (getPost["error"] !== undefined) {
-        throw Error("Failed to get post")
-    }
-
-    let getSubPostQuery = {
-        text: 
-        `select subposts.subpostid, subposts.postid, subposts.subpost, subposts.subpostdatetime, users.email from cryptown.subposts as subposts left join cryptown.users as users on subposts.userid=users.userid;`,
-        // values: [postId, escaped_userId, escaped_post, escaped_dateTime,server_datetime]
-    }
-
-    let getSubPost = await queryDb(getSubPostQuery)
-
-    if (getSubPost["error"] !== undefined) {
-        throw Error("Failed to get sub post")
-    }
-    
-    return {getPost: getPost["result"], getSubPost: getSubPost["result"]}
-
-}
-
-
-const addPost = async function(userId, post, dateTime) {
-    let escaped_userId = validator.escape(userId)
-    let escaped_post = validator.escape(post)
-    let escaped_dateTime = validator.escape(dateTime)
-    let server_datetime = new Date();
-    let postId = uuidv4()
-
-    let checkUser = {
-        text: "select * from cryptown.users where userid=$1",
-        values: [escaped_userId]
-    }
-
-    let user = await queryDb(checkUser)
-
-    if (user["result"].length === 0) {
-        throw Error('User does not exist')
-    }
-
-    let addPost = {
-        text: 
-        `insert into cryptown.posts 
-            (postid, userid, post, postdatetime, serverdatetime) 
-            values 
-            ($1, $2, $3, $4::timestamp, $5::timestamp);`,
-        values: [postId, escaped_userId, escaped_post, escaped_dateTime,server_datetime]
-    }
-
-    let postQuery = await queryDb(addPost)
-
-    if (postQuery["error"] !== undefined) {
-        throw Error("Failed to add post")
-    }
-    
-    return true
-
-}
-
-
-const addSubPost = async function(userId, postId, post, dateTime) {
-    //***** Add client side datetime *****//
-    let escaped_userId = validator.escape(userId)
-    let escaped_post = validator.escape(post)
-    let escaped_postId = validator.escape(postId)
-    let escaped_dataTime = validator.escape(dateTime)
-    let server_datetime = new Date();
-    let subPostId = uuidv4()
-
-    // check if that user exists 
-    let checkUser = {
-        text: "select * from cryptown.users where userid=$1",
-        values: [escaped_userId]
-    }
-
-    let user = await queryDb(checkUser)
-
-    if (user["result"].length === 0) {
-        throw Error('User does not exist')
-    }
-
-    // check if the main post exists
-    let checkMainPost = {
-        text: "select * from cryptown.posts where postid=$1",
-        values: [escaped_postId]
-    }
-
-    let mainPost = await queryDb(checkMainPost)
-
-    if (mainPost["result"].length === 0) {
-        throw Error('Post does not exist')
-    }
-
-    let addPost = {
-        text: 
-        `insert into cryptown.subposts 
-            (subpostid, postid, userid, subpost, subpostdatetime, serverdatetime) 
-            values 
-            ($1, $2, $3, $4, $5::timestamp, $6::timestamp);`,
-        values: [subPostId, escaped_postId, escaped_userId, escaped_post, escaped_dataTime, server_datetime]
-    }
-
-    let postQuery = await queryDb(addPost)
-
-    if (postQuery["error"] !== undefined) {
-        throw Error("Failed to add sub post db")
-    }
-    
-    return true
-
-}
 
 
 

20a87df6fa005a4917fb94b9bc00d0ba1a61352a
Tan Weng Onn <weng.onn@besquare.com.my>
2022-11-21 08:39:34 +0000

    logout

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -16,11 +16,11 @@ const requireAuth = async (req, res, next)  =>  {
         return res.status(401).json({ error: 'Authorization token required' })
     }
 
-    try{
+    // authorization header format
+    // Bearer <JWT token>
+    const jwtToken = authorization.split(' ')[1]
 
-        // authorization header format
-        // Bearer <JWT token>
-        const jwtToken = authorization.split(' ')[1]
+    try{
         const decryptedJwtToken = aesDecrypt(jwtToken, process.env.AES_PASS)  // getting the token 
         const { userId } = jwt.verify(decryptedJwtToken, publicKEY, { algorithm: "RS256" })
 
@@ -33,19 +33,19 @@ const requireAuth = async (req, res, next)  =>  {
         }
 
         let output = await queryDb(query)
+
+        if (output["result"].length === 0) {
+            throw Error() 
+        }
         
         let jwtQuery = {
             text: "select * from cryptown.jwt where userid=$1 and jwt=$2;",
             values: [escaped_userId, escapedJwtToken]
         }
 
-        console.log("USERID", escaped_userId)
-        console.log("JWT TOKEN", escapedJwtToken)
-
         let jwtOutput = await queryDb(jwtQuery)
 
         if (jwtOutput["result"].length === 0) {
-            console.log("User logged out")
             throw Error() 
         }
 
@@ -55,7 +55,37 @@ const requireAuth = async (req, res, next)  =>  {
         next()
 
     } catch (error) {
-        console.log(error)
+        if (error.name === 'TokenExpiredError') {
+            const decryptedJwtToken = aesDecrypt(jwtToken, process.env.AES_PASS)  // getting the token 
+            const { userId } = jwt.verify(decryptedJwtToken, publicKEY, { ignoreExpiration: true, algorithm: "RS256" })
+
+            const escapedJwtToken = validator.escape(decryptedJwtToken)
+            const escaped_userId = validator.escape(userId)
+
+            let jwtQuery = {
+                text: "select * from cryptown.jwt where userid=$1 and jwt=$2;",
+                values: [escaped_userId, escapedJwtToken]
+            }
+
+            let jwtOutput = await queryDb(jwtQuery)
+
+            if (jwtOutput["result"].length !== 0) {
+
+                let deleteJwt = {
+                    text: 
+                    `delete from cryptown.jwt where jwt=$1 and userid=$2;`,
+                    values: [escapedJwtToken, escaped_userId]
+                }
+            
+                let deleteJwtOutput = await queryDb(deleteJwt)
+            
+                if (deleteJwtOutput["error"] !== undefined) {
+                    // logger.warn({ label:'Favourite API', message: `Failed to delete favourite - ${escaped_favId}`, outcome:'failed', user: escaped_userId, ipAddress: req.ip})
+                    throw Error("Failed to delete jwt")
+                }
+            }
+        }
+        
         res.status(401).json({ error: 'Request is not authorized' })
     }
 
